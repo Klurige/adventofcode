bbc2369781d63878ad9856cceb7f28d8ec0b6893
Robert Niemi <robert.den.klurige@gmail.com>
2022-11-30 14:54:37 +0100

    Adds option to run specific year.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,10 +7,15 @@ import kotlin.system.exitProcess
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
-// Set to true to run all exercises.
-// If false, only one will be executed, or if it doesn't exist - created.
-// (Nothing will be created if RUN_ALL == true)
-const val RUN_ALL = false
+// Normally negative, which means RUN_TODAY or RUN_DATE.
+// Set to 0 to run every task, or a specific year for all tasks from that year.
+const val RUN_ALL = -1
+
+// Set to true to run today's exercise. (RUN_ALL takes precedence.)
+const val RUN_TODAY = true
+
+// Run exercise for date. (Only if RUN_ALL and RUN_TODAY are both false.)
+const val RUN_DATE = "2022-12-01"
 
 fun main(args: Array<String>) {
     if (args.size != 1) {
@@ -18,44 +23,47 @@ fun main(args: Array<String>) {
         exitProcess(1)
     }
 
-    var date: LocalDate? = null
-
-    // Uncomment this and change date to run that specific date.
-    date = LocalDate.parse("2022-12-01")
-
-    @Suppress("KotlinConstantConditions")
-    if (date == null) date = LocalDate.now()
-
-    if (RUN_ALL) {
-        (2015..LocalDate.now().year).forEach { year ->
-            (1..25).forEach { day ->
-                date = LocalDate.parse("$year-12-${String.format("%02d", day)}")
+    when {
+        RUN_ALL < 0 -> {
+            val date = if (RUN_TODAY) {
+                LocalDate.now()
+            } else {
+                LocalDate.parse(RUN_DATE)
+            }
+            if (date.month != Month.DECEMBER || date.dayOfMonth > 25) {
+                println("Date $date is not in advent.")
+            } else {
                 executeExercise(date!!, args[0])
-
             }
         }
-    } else {
-        executeExercise(date!!, args[0])
+
+        RUN_ALL == 0 -> {
+            (2015..LocalDate.now().year).forEach { year ->
+                (1..25).forEach { day ->
+                    executeExercise(LocalDate.parse("$year-12-${String.format("%02d", day)}"), args[0])
+                }
+            }
+        }
+
+        else -> {
+            (1..25).forEach { day ->
+                executeExercise(LocalDate.parse("$RUN_ALL-12-${String.format("%02d", day)}"), args[0])
+            }
+        }
     }
 }
 
 @OptIn(ExperimentalTime::class)
 fun executeExercise(date: LocalDate, flavour: String) {
-    val username = System.getProperty("user.name")
-
     runCatching {
-        if (date.month != Month.DECEMBER || date.dayOfMonth > 25) {
-            println("Date is not in advent.")
-            throw NullPointerException()
-        }
-         val cl = ::main.javaClass.classLoader.loadClass("$username.y${date.year}.d${date.dayOfMonth}.DayKt")
+        val cl = ::main.javaClass.classLoader.loadClass("y${date.year}.d${date.dayOfMonth}.DayKt")
         val m = cl.methods.find { it.name == "day" && Modifier.isStatic(it.modifiers) }
         m?.kotlinFunction ?: throw NullPointerException()
     }.onSuccess {
         println("Running ${date.year} - Day${date.dayOfMonth}")
         val data = mutableListOf<String>()
         runCatching {
-            File("src/main/kotlin/$username/y${date.year}/d${date.dayOfMonth}/$flavour.txt").useLines { lines ->
+            File("src/main/kotlin/y${date.year}/d${date.dayOfMonth}/$flavour.txt").useLines { lines ->
                 lines.forEach {
                     data.add(it)
                 }
@@ -70,21 +78,21 @@ fun executeExercise(date: LocalDate, flavour: String) {
         println("Execution took $elapsed")
         println()
     }.onFailure {
-        if (!RUN_ALL) {
-            println("Couldn't find $username.y${date.year}.d${date.dayOfMonth}()")
+        if (RUN_ALL < 0) {
+            println("Couldn't find y${date.year}.d${date.dayOfMonth}()")
 
             if (date.month == Month.DECEMBER && date.dayOfMonth <= 25) {
-                val dir = File("src/main/kotlin/$username/", "y${date.year}/d${date.dayOfMonth}")
+                val dir = File("src/main/kotlin/", "y${date.year}/d${date.dayOfMonth}")
                 dir.mkdirs()
                 File(dir, "input.txt").createNewFile()
                 File(dir, "test.txt").createNewFile()
                 val src = File(dir, "Day.kt")
                 src.createNewFile()
                 src.printWriter().use {
-                    it.println("package $username.y${date.year}.d${date.dayOfMonth}")
+                    it.println("package y${date.year}.d${date.dayOfMonth}")
                     it.println("")
                     it.println("fun day(data: List<String>) {")
-                    it.println("")
+                    it.println("    println(\"${date}\")")
                     it.println("}")
                 }
                 println()
